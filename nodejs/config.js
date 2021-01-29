881737a0239fc7d1246a2b797d6bbf14939c278d
qcsjin <56754660+qcsjin@users.noreply.github.com>
2021-01-29 13:18:44 +0800

    Update config.js

--- nodejs/config.js
+++ nodejs/config.js
@@ -19,11 +19,11 @@ const config = {
 
     // This information is used to issue your "Government ID"
     userInformation: {
-        name: process.env.NAME || 'Alice Garcia',
-        email: process.env.EMAIL || 'alice@faber.edu',
+        name: process.env.NAME || 'Stitch',
+        email: process.env.EMAIL || '123456789@qq.com',
         tax_id: process.env.TAX_ID || '123-45-6789',
         icon_src: process.env.ICON_SRC || 'https://cdn3.iconfinder.com/data/icons/black-easy/512/538738-school_512x512.png',
-        username: process.env.USERNAME || 'alice',
+        username: process.env.USERNAME || 'Stitch',
         password: process.env.PASSWORD || '123'
     },
 
